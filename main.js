9bfabd42301dc52c0901a23c80d861e007b80195
Aaron Larner <anlarner@gmail.com>
2015-10-06 16:08:00 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+'use strict';
+Parse.initialize("b1vJxODKeikf774DQsI1oYCBK4awXkcuK10WS9vY", "Epc1u39Gr5jqK7fE9dOHxWrBxDcfJkoIDihLKFVd");
+var Message = Parse.Object.extend('Message');
+var query = new Parse.Query(Message);
